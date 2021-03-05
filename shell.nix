7a918d2ce6d574cc9d296f31eaf18b472d6214c2
Tristan Gosselin-Hane <starcraft66@gmail.com>
2021-03-05 12:08:43 -0500

    Add nix-shell

---
+++ shell.nix
@@ -0,0 +1,22 @@
+{ pkgs ? import (builtins.fetchTarball "https://github.com/nixos/nixpkgs/archive/176690a7630f4ec253bba538fe24086750decc6e.tar.gz") { }
+}:
+
+with pkgs;
+mkShell rec {
+  buildInputs = [
+    jsonnet
+    jsonnet-bundler
+    gojsontoyaml
+  ];
+
+  shellHook = let
+    concatVersions = x: lib.concatStringsSep "\n" (lib.flatten x);
+    mapper = builtins.map (x: if builtins.typeOf x == "list" then concatVersions (mapper x) else x.name);
+    versions = mapper buildInputs;
+  in ''
+    getVersion () {
+      echo "${concatVersions versions}"
+    }
+    getVersion
+  '';
+}
